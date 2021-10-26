0cdb9ea5be3b53326472d95bc0ba904b7b8028b6
Roshni <sparksroshni610@gmail.com>
2021-10-26 23:33:09 +0530

    dropdown

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className="container">
     <Mycartcontainer/>
     </div>
-    <CartDropdown/>
+    
     </Fragment>
       );
 }
--- src/Components/CartDropdown.jsx
+++ src/Components/CartDropdown.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import "./cartdropdown.css"
-const CartDropdown = () => {
+import { FaTrash} from "react-icons/fa";
+import { Fragment } from 'react/cjs/react.production.min';
+import "./cartdropdown.css" 
+const CartDropdown = ({toggleHeart,toggleCart}) => {
     return (
-        <ul className="cartdropdown">
-            <li><span>Beef</span><span>$400</span></li>
-            <li><span>Beef</span><span>$400</span></li>
+       
+          
+            <ul className={`cartdropdown ${toggleHeart?"":"hidden"}`}>
+            <li><span>Beef</span><span>$400</span><span><FaTrash/></span></li>
+            <li><span>Beef</span><span>$400</span><span><FaTrash/></span></li>
+            <li><span>Beef</span><span>$400</span><span><FaTrash/></span></li>
+            <li><span>Beef</span><span>$400</span><span><FaTrash/></span></li>
+            <li><span>Beef</span><span>$400</span><span><FaTrash/></span></li>
+            <li><span>Beef</span><span>$400</span><span><FaTrash/></span></li>
+            <li><span>Beef</span><span>$400</span><span><FaTrash/></span></li>
+            <li><span>Beef</span><span>$400</span><span><FaTrash/></span></li>
+            <li><span>Beef</span><span>$400</span><span><FaTrash/></span></li>
+            
 
         </ul>
+        
+       
+        
+        
+        
     )
 }
 
